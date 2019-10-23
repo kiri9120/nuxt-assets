ddfa4332c71934d66411ee4b1a4c58eaf445ce81
Masato Kiryu <kiri9120@gmail.com>
2019-10-23 19:14:09 +0900

    update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/nuxt-assets/favicon.ico' }
     ]
   },
   /*
@@ -48,5 +48,8 @@ export default {
     */
     extend (config, ctx) {
     }
+  },
+  router: {
+    base: '/nuxt-assets/'
   }
 }
